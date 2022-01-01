8c6fc0d269427338bb92940a9b867d7d9dad3435
Charlie Dedinsky <dedin005@umn.edu>
2022-01-01 16:56:54 -0600

    Push Update

---
+++ copykeys.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ssh-copy-id user@192.168.1.178
+ssh-copy-id user@192.168.1.235
+ssh-copy-id user@192.168.1.108
+ssh-copy-id user@192.168.1.173
+ssh-copy-id user@192.168.1.237
+ssh-copy-id user@192.168.1.174
+ssh-copy-id user@192.168.1.137
+ssh-copy-id user@192.168.1.177
