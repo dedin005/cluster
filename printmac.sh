675fb399aa4f6d0a598acf4a4cfc45cb754adc98
Charlie Dedinsky <dedin005@umn.edu>
2022-01-01 17:05:08 -0600

    changed printmac to be more verbose

--- printmac.sh
+++ printmac.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# ssh user@192.168.1.178 cat /sys/class/net/eno1/address && echo 'master'
-# ssh user@192.168.1.235 cat /sys/class/net/eno1/address && echo 'node1'
-# ssh user@192.168.1.108 cat /sys/class/net/eno1/address && echo 'node2'
-# ssh user@192.168.1.173 cat /sys/class/net/eno1/address && echo 'node3'
-# ssh user@192.168.1.237 cat /sys/class/net/eno1/address && echo 'node4'
-# ssh user@192.168.1.174 cat /sys/class/net/eno1/address && echo 'node5'
-# ssh user@192.168.1.137 cat /sys/class/net/eno1/address && echo 'node6'
-# ssh user@192.168.1.177 cat /sys/class/net/eno1/address && echo 'node7'
-ssh user@192.168.1.178 ssh-keygen 
-ssh user@192.168.1.235 ssh-keygen
-ssh user@192.168.1.108 ssh-keygen
-ssh user@192.168.1.173 ssh-keygen
-ssh user@192.168.1.237 ssh-keygen
-ssh user@192.168.1.174 ssh-keygen
-ssh user@192.168.1.137 ssh-keygen
-ssh user@192.168.1.177 ssh-keygen
+## This part lets the user run './printmac.sh > macAddresses' to get a list of all the macs ##
+
+# ssh user@192.168.1.178 cat /sys/class/net/eno1/address && hostname
+# ssh user@192.168.1.235 cat /sys/class/net/eno1/address && hostname
+# ssh user@192.168.1.108 cat /sys/class/net/eno1/address && hostname
+# ssh user@192.168.1.173 cat /sys/class/net/eno1/address && hostname
+# ssh user@192.168.1.237 cat /sys/class/net/eno1/address && hostname
+# ssh user@192.168.1.174 cat /sys/class/net/eno1/address && hostname
+# ssh user@192.168.1.137 cat /sys/class/net/eno1/address && hostname
+# ssh user@192.168.1.177 cat /sys/class/net/eno1/address && hostname
+
+## This part generates ssh keys for the nodes ##
+
+# ssh user@192.168.1.178 ssh-keygen 
+# ssh user@192.168.1.235 ssh-keygen
+# ssh user@192.168.1.108 ssh-keygen
+# ssh user@192.168.1.173 ssh-keygen
+# ssh user@192.168.1.237 ssh-keygen
+# ssh user@192.168.1.174 ssh-keygen
+# ssh user@192.168.1.137 ssh-keygen
+# ssh user@192.168.1.177 ssh-keygen
 
